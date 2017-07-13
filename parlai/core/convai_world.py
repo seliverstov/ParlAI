c4154b31ca44045e131f3d37593e87e2c27d5070
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-07-13 17:44:34 +0300

    Refactor convai world

--- parlai/core/convai_world.py
+++ parlai/core/convai_world.py
@@ -7,6 +7,7 @@
 from parlai.core.worlds import World
 from parlai.core.agents import create_agent_from_shared
 from parlai.core.worlds import validate
+from parlai.core.agents import Agent
 
 import requests
 import os
@@ -15,8 +16,8 @@ import time
 
 
 class ConvAIWorld(World):
-    """This world send messages from agents (client bots)  back and forth to ConvAI Router Bot.
-    Agents created dynamically when new conversation is started. Each agent agent conducts one conversation.
+    """This world send messages from agents (client bots)  back and forth to ConvAI Master Bot.
+    Agents created dynamically from shared data when new conversation is started. Each agent conducts one conversation.
     """
 
     def __init__(self, opt, agents, shared=None):
@@ -31,26 +32,29 @@ class ConvAIWorld(World):
         self.chats = {}
 
         self.router_bot_url = opt.get('router_bot_url')
+
         self.router_bot_pull_delay = int(opt.get('router_bot_pull_delay'))
         if self.router_bot_pull_delay < 1:
             self.router_bot_pull_delay = 1
 
         self.bot_id = opt.get('bot_id')
-        self.bot_capacity = opt.get('bot_capacity')
+
+        self.bot_capacity = int(opt.get('bot_capacity'))
+
         self.bot_url = os.path.join(self.router_bot_url, self.bot_id)
 
-    def __get_updates(self):
+    def _get_updates(self):
         res = requests.get(os.path.join(self.bot_url, 'getUpdates'))
         return res.json()
 
-    def __send_message(self, observation, chat):
+    def _send_message(self, observation, chat):
         if self._is_end_of_conversation(observation['text']):
             data = {
                 'text': '/end',
                 'evaluation': {
-                    'quality': int(input("How do you evaluate quality of conversation (form 1 to 10)? ")),
-                    'breadth': int(input("How do you evaluate breadth of conversation (form 1 to 10)? ")),
-                    'engagement': int(input("How do you evaluate engagement of conversation (form 1 to 10)? "))
+                    'quality': 0,
+                    'breadth': 0,
+                    'engagement': 0
                 }
             }
         else:
@@ -78,7 +82,11 @@ class ConvAIWorld(World):
 
     @staticmethod
     def _is_end_of_conversation(message):
-        return message == '/end' or message == ''
+        return message == '/end'
+
+    @staticmethod
+    def _is_skip_response(message):
+        return message == ''
 
     @staticmethod
     def _get_chat_id(message):
@@ -86,8 +94,7 @@ class ConvAIWorld(World):
 
     @staticmethod
     def _get_message_text(message):
-        raw_text = message['message']['text'].replace('\\', '/')
-        return json.loads(raw_text)['text']
+        return message['message']['text']
 
     @staticmethod
     def _strip_start_message(message):
@@ -96,69 +103,98 @@ class ConvAIWorld(World):
     def _init_chat(self, chat):
         agent = create_agent_from_shared(self.shared["agents"][0])
         self.chats[chat] = agent
-        self.agents.append(agent)
         print("New chat #%s created and corresponding agent added." % chat)
-        return agent
-
-    def _cleanup_chat(self, chat):
-        agent = self.chats.pop(chat, None)
-        self.agents.remove(agent)
-        print("Chat #% is ended and corresponding agent is removed.")
         return agent
 
     def _cleanup_chats(self, chats):
         for chat in chats:
-            self._cleanup_chat(chat)
+            self.chats.pop(chat, None)
+            print("Chat #%s is ended and corresponding agent is removed." % chat)
 
     def parley(self):
-        print("ConvAIWorld:parley " + "-" * 100)
+        print("\n"+"-" * 100+"\n")
+        '''
+        Send new messages to agents
+        '''
+        msgs = self._get_updates()
+        active_chats = set()
+        finished_chats = set()
         acts = []
-        msgs = self.__get_updates()
-        current_chats = {}
+        agents = []
         for msg in msgs:
             print("Proceed message: %s" % msg)
             text = self._get_message_text(msg)
             chat = self._get_chat_id(msg)
             episode_done = False
-            agent = None
+
             if self._is_begin_of_conversation(text):
                 print("Message recognised as start of new conversation #%s" % chat)
-                if 0 <= int(self.bot_capacity) > len(self.agents):
-                    agent = self._init_chat(chat)
+
+                if self.chats.get(chat, None) is not None:
+                    print("WARNING: Chat #%s already exists and it will be overwritten!")
+                else:
+                    pass
+
+                if self.bot_capacity == -1 or 0 <= self.bot_capacity > len(self.chats):
+                    self._init_chat(chat)
                     text = self._strip_start_message(text)
                 else:
                     print("Can't start new conversation #%s due to bot capacity limit reached." % chat)
             elif self._is_end_of_conversation(text):
                 print("Message recognised as end of conversation #%s" % chat)
-                agent = self._cleanup_chat(chat)
                 episode_done = True
+                finished_chats.add(chat)
             else:
-                agent = self.chats[chat]
-                if agent is not None:
-                    print("Message was recognized as part of chat #%s" % chat)
-                else:
-                    print("Message wasn't recognized as part of any chat. Message skipped.")
+                pass
+
+            agent = self.chats.get(chat, None)
 
             if agent is not None:
+                print("Message was recognized as part of chat #%s" % chat)
+                active_chats.add(chat)
+                stub_agent = StubConvAiAgent({'chat': chat})
                 m = {
-                    'id': 'RouterBot#%s' % chat,
+                    'id': stub_agent.id,
                     'text': text,
                     'episode_done': episode_done
                 }
-                print("Send message to agent: %s" % m)
+                print("Send new message from chat #%s to corresponding agent %s" % (chat, agent))
                 agent.observe(validate(m))
                 acts.append(m)
-                current_chats[chat] = agent
+                agents.append(stub_agent)
+            else:
+                print("Message wasn't recognized as part of any chat. Message skipped.")
 
-        chats_to_cleanup = []
-        for (chat, agent) in current_chats.items():
-            act = validate(agent.act())
-            print("Send response from agent for conversation %s: %s" % (chat, act))
-            self.__send_message(act, chat)
-            acts.append(act)
-            if self._is_end_of_conversation(act['text']) or act['episode_done']:
-                chats_to_cleanup.append(chat)
-        self._cleanup_chats(chats_to_cleanup)
+        '''
+        Collect responses from agents
+        '''
+        for chat in active_chats.difference(finished_chats):
+            agent = self.chats.get(chat, None)
+            if agent is not None:
+                act = validate(agent.act())
+
+                if self._is_end_of_conversation(act['text']) or act['episode_done']:
+                    finished_chats.add(chat)
+                else:
+                    pass
+
+                if self._is_skip_response(act['text']):
+                    print("Skip response from agent for conversation #%s" % chat)
+                else:
+                    print("Send response from agent for conversation #%s: %s" % (chat, act))
+                    self._send_message(act, chat)
+                    acts.append(act)
+                    agents.append(agent)
+
+        self.acts = acts
+        self.agents = agents
+        '''
+        Cleanup finished chats
+        '''
+        self._cleanup_chats(finished_chats)
+        '''
+        Sleep before new pull request 
+        '''
         print("Sleep for %s seconds before new round of conversation" % self.router_bot_pull_delay)
         time.sleep(self.router_bot_pull_delay)
 
@@ -171,3 +207,16 @@ class ConvAIWorld(World):
     def shutdown(self):
         for (chat, agent) in self.chats.items():
             agent.shutdown()
+
+
+class StubConvAiAgent(Agent):
+    def __init__(self, opt, shared=None):
+        super().__init__(opt, shared)
+        self.id = "MasterBot#%s" % opt['chat']
+
+    def act(self):
+        pass
+
+    def observe(self, observation):
+        pass
+
