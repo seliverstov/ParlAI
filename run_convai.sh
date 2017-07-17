415c2aa95d8b673d6abed665f9abdf8e086ce3fc
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-07-17 12:29:12 +0300

    Change master bot url

--- run_convai.sh
+++ run_convai.sh
@@ -1,2 +1,2 @@
 python setup.py develop
-python examples/convai.py -bi=0000000 -rbu=https://convaibot.herokuapp.com/ -rbpd=1
+python examples/convai.py -bi=0000000 -rbu=https://radiant-dusk-86898.herokuapp.com/ -rbpd=1
